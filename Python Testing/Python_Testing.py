9b383b395cb1e6dc48f4bc492c279c899f90893e
Damon West <DamonSWest@Gmail.com>
2022-02-11 14:11:52 -0500

    f

--- Python Testing/Python_Testing.py
+++ Python Testing/Python_Testing.py
@@ -1,6 +1,15 @@
-def 
-
+import time
+number = 0
+iteration = 0
+def numbers():
+    number = input("What is your number?\n")
+    number = int(number)
+    print("Starting...")
+    for x in range(iteration, number):
+        time.sleep(1)
+        print(number)
+    time.sleep(1)
 def main():
-    print("This is running code!")
-
+    print("Hello!")
+    numbers()
 main()
